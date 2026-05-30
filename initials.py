a5f4415bcb8ef7c44fbb5b0a98de8ed6a17ec811
nanayaw666 <2425401455@live.gctu.edu.gh>
2026-05-30 22:06:16 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Desmond Afoakwa Nana Yaw
+# STUDENT ID: 2425401455
+# Email: 2425401455@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Desmond Afoakwa Nana Yaw, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+   # Prints initials D, A, N, Y in large block letters (12 wide x 10 tall each).
 
+    print()
+    print("...DANY")
+    print()
+    print("...DDDDDDDDD...............AAAA............NN........NN........YY........YY.....")
+    print("...DD........DD...........AA..AA...........NNN.......NN.........YY......YY......")
+    print("...DD........DD..........AA....AA..........NNNN......NN..........YY....YY.......")
+    print("...DD........DD.........AA......AA.........NN.NN.....NN...........YY..YY........")
+    print("...DD........DD........AAAAAAAAAAAA........NN..NN....NN............YYYY.........")
+    print("...DD........DD........AA........AA........NN...NN...NN.............YY..........")
+    print("...DD........DD........AA........AA........NN....NN..NN.............YY..........")
+    print("...DD........DD........AA........AA........NN.....NNNNN.............YY..........")
+    print("...DD........DD...**...AA........AA...**...NN......NNNN...**........YY........**")
+    print("...DDDDDDDDD......**...AA........AA...**...NN.......NNN...**........YY........**")
+    print()
 
 
 main()
